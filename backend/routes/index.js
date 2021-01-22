9c669afc775fff03f89119cb4fb12c082fac6e98
jamie <jzrust@gmail.com>
2021-01-22 15:40:43 -0500

    finished phase 2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
 
 module.exports = router;
