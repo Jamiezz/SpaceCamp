afe1ef9d3c73387165daa0249d375cc3b49b8b08
jamie <jzrust@gmail.com>
2021-01-23 18:43:53 -0500

    initial set up phase 2, phase 1 not complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,14 +1,67 @@
 // backend/routes/api/index.js
- const router = require('express').Router();
-
-
-
+const router = require('express').Router();
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 // backend/routes/api/index.js
 // ...
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
+
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// backend/routes/api/index.js
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+module.exports = router;
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 // ...
 
