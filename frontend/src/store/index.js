e3edf7e05ce82fcd43a2039038b7b69e6dcf1440
jamie <jzrust@gmail.com>
2021-01-26 15:23:58 -0500

    previous commit was backend solution, phase 3 phase 3 now

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,33 +1,23 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
-import React from 'react';
-
-import './index.css';
-
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
-
-import configureStore from './store';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import session from './session'
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session
 });
 
 let enhancer;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require("redux-logger").default;
+  const logger = require('redux-logger').default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-configureStore = (preloadedState) => {
+const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,75 +1,69 @@
-// frontend/src/store/session.js
-import { fetch } from './csrf';
+import { fetch } from './csrf.js';
 
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
 
-const setUser = (user) => {
-  return {
-    type: SET_USER,
-    payload: user,
-  };
+const setUser = (user) => ({
+  type: SET_USER,
+  payload: user
+});
+
+const removeUser = () => ({
+  type: REMOVE_USER
+});
+
+export const login = ({ credential, password }) => async (dispatch) => {
+  const res = await fetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password })
+  });
+  dispatch(setUser(res.data.user));
+  return res;
 };
 
-const removeUser = () => {
-  return {
-    type: REMOVE_USER,
-  };
-};
-
-export const login = (user) => {
-  return async (dispatch) => {
-    const { credential, password } = user;
-    const response = await fetch('/api/session', {
-      method: 'POST',
-      body: JSON.stringify({
-        credential,
-        password,
-      }),
-    });
-    dispatch(setUser(response.data.user));
-    return response;
-  }
-}
-
-// frontend/src/store/session.js
-// ...
-export const restoreUser = () => async dispatch => {
+export const restoreUser = () => async (dispatch) => {
   const res = await fetch('/api/session');
   dispatch(setUser(res.data.user));
   return res;
 };
-// ...
 
-const initialState = { user: null };
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    })
+  });
 
-// frontend/src/store/session.js
-// ...
+  dispatch(setUser(response.data.user));
+  return response;
+};
+
 export const logout = () => async (dispatch) => {
   const response = await fetch('/api/session', {
-    method: 'DELETE',
+    method: 'DELETE'
   });
   dispatch(removeUser());
   return response;
 };
-// ...
 
-const sessionReducer = (state = initialState, action) => {
+const initialState = { user: null };
+
+function reducer(state = initialState, action) {
   let newState;
   switch (action.type) {
     case SET_USER:
-      newState = Object.assign({}, state);
-      newState.user = action.payload;
+      newState = Object.assign({}, state, { user: action.payload });
       return newState;
     case REMOVE_USER:
-      newState = Object.assign({}, state);
-      newState.user = null;
+      newState = Object.assign({}, state, { user: null });
       return newState;
     default:
       return state;
   }
-};
+}
 
-
-
-export default sessionReducer;
+export default reducer;
