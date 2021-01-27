e74ad6da4cb25ea92c4ee9b75d3543ce659085c3
jamie <jzrust@gmail.com>
2021-01-27 18:51:14 -0500

    added a bit of css to map route form

--- frontend/src/components/expeditionFormPage/index.js
+++ frontend/src/components/expeditionFormPage/index.js
@@ -7,17 +7,17 @@ const ExpeditionInput = () => {
   const [body, setBody] = useState('');
   const [map, setMap] = useState('');
 
-  useEffect(() => {
-    homeFetch()
-  }, [])
+  //   useEffect(() => {
+  //     homeFetch()
+  //   }, [])
 
-async function homeFetch() {
-  const res = await fetch("/api/map")
-  if(res.ok) {
-    let data = await res.data
-    setMap(data)
-  }
-}
+  // async function homeFetch() {
+  //   const res = await fetch("/api/map")
+  //   if(res.ok) {
+  //     let data = await res.data
+  //     setMap(data)
+  //   }
+  // }
 
   const handleSubmit = (e) => {
     console.log('handleSubmit clicked');
@@ -27,21 +27,28 @@ async function homeFetch() {
     <div className='inputBox'>
       <h1>Map Expedition</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-          placeholder='Title'
-          name='title'
-        />
-
-        <textarea
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          name='body'
-          placeholder='Add your entry'
-        ></textarea>
-        <button type='submit'>Lift off</button>
+        <div className='color'>
+          <input
+            className="textField"
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            placeholder='Title'
+            name='title'
+          />
+        </div>
+        <div className='color'>
+          <textarea
+            className="textField"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            name='body'
+            placeholder='Add your entry'
+          ></textarea>
+          <div>
+            <button type='submit' className="button">Lift off</button>
+          </div>
+        </div>
       </form>
     </div>
   );
